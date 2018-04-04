bc7051155ae5c1bca527f6c5e42fdc00609ff277
Louisa Xue <xuelex@gmail.com>
2018-04-03 18:42:24 -0700

    Construct MinMaxStackQueue

--- max_windowed_range.rb
+++ max_windowed_range.rb
@@ -46,11 +46,11 @@ class MyStack
     @store = []
   end
 
-  def stack(val)
+  def push(val)
     @store.push(val)
   end
 
-  def destack
+  def pop
     @store.pop
   end
 
@@ -64,5 +64,122 @@ class MyStack
 
   def empty?
     @store.empty?
+  end
+end
+
+class StackQueue
+  def initialize
+    @in_stack = MyStack.new
+    @out_stack = MyStack.new
+  end
+
+  def enqueue
+    @in_stack.push(val)
+  end
+
+  def dequeue
+    queueify if @out_stack.empty?
+    @out_stack.pop
+  end
+
+  def empty?
+    @out_stack.empty? && @in_stack.empty?
+  end
+
+  def size
+    @out_stack.size + @in_stack.size
+  end
+
+
+  private
+
+  def queuify
+    @out_stack.push(@in_stack.pop) until @in_stack.empty?
+  end
+end
+
+class MinMaxStack
+  def initialize
+    @store = MyStack.new
+  end
+
+  def empty?
+    @store.empty?
+  end
+
+  def peek
+    @store.peek[:value]
+  end
+
+  def max
+    @store.peek[:max]
+  end
+
+  def min
+    @store.peek[:min]
+  end
+
+  def push(val)
+    @store.push({
+      max: new_max(val),
+      min: new_min(val),
+      value: val
+      })
+  end
+
+
+  private
+
+  def new_max
+    empty? ? val : [max_val].max
+  end
+
+  def new_min
+    empty? ? val : [max_val].min
+  end
+
+end
+
+class MinMaxStackQueue
+
+  def initialize
+    @in_stack = MinMaxStack.new
+    @out_stack = MinMaxStack.new
+  end
+
+  def enqueue
+    @in_stack.push(val)
+  end
+
+  def dequeue
+    queueify if @out_stack.empty?
+    @out_stack.pop
+  end
+
+  def empty?
+    @out_stack.empty? && @in_stack.empty?
+  end
+
+  def size
+    @out_stack.size + @in_stack.size
+  end
+
+  def max
+    maxes = []
+    maxes << @in_stack.max unless @in_stack.empty?
+    maxes << @out_stack.max unless @out_stack.empty?
+  end
+
+  def min
+    mins = []
+    mins << @in_stack.min unless @in_stack.empty?
+    mins << @out_stack.min unless @out_stack.empty?
+  end
+
+
+  private
+
+  def queuify
+    @out_stack.push(@in_stack.pop) until @in_stack.empty?
   end
 end
